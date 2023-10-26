38aedd9e5a78facb7baa7e1982b33ac1c7bb79e7
qiqicodes <68657634+qiqicodes@users.noreply.github.com>
2023-10-25 21:35:02 -0700

    experimenting with chakra components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,44 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  ChakraProvider,
+  Container,
+  Heading,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 
 export default function Page() {
   return (
     <ChakraProvider>
-      <h1>Hello, Snail!</h1>
-      <p>hi hi hi</p>
+      <Container maxW="2xl" centerContent height="xl">
+        <Box padding="4" bg="salmon" color="white" maxW="md">
+          <Heading as="h1" size="3xl" noOfLines={1}>
+            Hello, Snail!
+          </Heading>
+          <Text fontSize="xl">hi hi hi</Text>
+        </Box>
+        <Box
+          padding="4"
+          bg="papayawhip"
+          color="purple"
+          maxW="md"
+          width="100%"
+          height="100%"
+        >
+          <Text>Type Here</Text>
+          <Stack direction="row" spacing={4}>
+            <Button
+              isLoading
+              loadingText="Submitting"
+              colorScheme="purple"
+              variant="solid"
+            >
+              Submit
+            </Button>
+          </Stack>
+        </Box>
+      </Container>
     </ChakraProvider>
   );
 }
